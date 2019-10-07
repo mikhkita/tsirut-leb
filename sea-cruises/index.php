e1cfaed3c42078f97d39559d379dac15fab5ee93
Rom4eS <rom4es_1993@mail.ru>
2019-10-07 11:28:34 +0700

    123

--- sea-cruises/index.php
+++ sea-cruises/index.php
@@ -1,5 +1,13 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Морские круизы");
-?><h2 class="b-title">Раздел в разработке</h2>
+?>
+	<script src="https://gocruise.ru/outside/" type="text/javascript"></script>
+	<script type="text/javascript">
+		cesSettings = {
+			email : 'belturist-info[dog]yandex.ru',
+			defaultPage : 'index',
+			isRur : 1
+		};
+	</script>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
